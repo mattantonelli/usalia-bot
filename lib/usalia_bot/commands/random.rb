8fb1f98108d1cf93101ac1ab9b095bcc6178e3fe
Matt Antonelli <mattr.antonelli@gmail.com>
2017-05-20 14:20:45 -0400

    Updated random to use the author's display name
    * If the author has a nickname, that will be used over their username

--- lib/usalia_bot/commands/random.rb
+++ lib/usalia_bot/commands/random.rb
@@ -2,9 +2,10 @@ module UsaliaBot
   module Commands
     module Random
       extend Discordrb::Commands::CommandContainer
+      extend UsaliaBot::HelperMethods
 
       command(:random, description: 'Roll a random number from 1 to 999.', usage: 'random') do |event|
-        user = event.message.author.name
+        user = display_name(event.author)
         result = rand(1..999)
         "Random! #{user} rolls a #{result}."
       end
--- lib/usalia_bot/helper_methods.rb
+++ lib/usalia_bot/helper_methods.rb
@@ -1,5 +1,9 @@
 module UsaliaBot
   module HelperMethods
     MENTION = /<@!?#{CLIENT_ID}>/
+
+    def display_name(user)
+      user.nickname || user.username
+    end
   end
 end
