d9ca397c46290dcda191501209aefa055eab82ac
Matt Antonelli <mattr.antonelli@gmail.com>
2017-05-21 21:00:43 -0400

    Removed display_name helper
    * Only properly worked for members anyway

--- lib/usalia_bot/commands/random.rb
+++ lib/usalia_bot/commands/random.rb
@@ -5,7 +5,7 @@ module UsaliaBot
       extend UsaliaBot::HelperMethods
 
       command(:random, description: 'Roll a random number from 1 to 999.', usage: 'random') do |event|
-        user = display_name(event.author)
+        user = event.author.display_name
         result = rand(1..999)
         "Random! #{user} rolls a #{result}."
       end
--- lib/usalia_bot/helper_methods.rb
+++ lib/usalia_bot/helper_methods.rb
@@ -2,10 +2,6 @@ module UsaliaBot
   module HelperMethods
     MENTION = /<@!?#{CLIENT_ID}>/
 
-    def display_name(user)
-      user.nickname || user.username
-    end
-
     def role_mention_members(message, readable = false)
       members = message.role_mentions.flat_map(&:members)
       members.keep_if { |member| member.permission?(:read_messages, message.channel) } if readable
