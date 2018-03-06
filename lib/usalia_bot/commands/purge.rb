14d0e82f2737809c8a9b7d68d0df832b340131c8
Matt Antonelli <mattr.antonelli@gmail.com>
2018-03-06 18:17:02 -0500

    Added purgeto command
    * Purges to a specific message ID instead of purging a # of messages
    * Minor refactoring of the original command

--- lib/usalia_bot/commands/purge.rb
+++ lib/usalia_bot/commands/purge.rb
@@ -4,18 +4,34 @@ module UsaliaBot
       extend Discordrb::Commands::CommandContainer
       extend UsaliaBot::HelperMethods
 
+      # Purge the given number of messages
       command(:purge, help_available: false, required_permissions: [:manage_messages],
               permission_message: false) do |event, number|
-        purge_amount = number.to_i + 1
+        amount = number.to_i + 1
 
-        if purge_amount.between?(2, 100)
+        if amount.between?(2, 100)
           begin
-            event.channel.prune(purge_amount, true)
+            event.channel.prune(amount, true)
           rescue ArgumentError
             reply = event.message.reply('You cannot purge messages older than 2 weeks, plip!')
           end
         else
           reply = event.message.reply('Please give me a number between 1 and 99, plip! (e.g. purge 10)')
+        end
+
+        delete_request(event.message, reply, 10) if reply
+      end
+
+      # Purge all messages up to and including the given message ID (max 100)
+      command(:purgeto, help_available: false, required_permissions: [:manage_messages],
+              permission_message: false) do |event, message_id|
+        begin
+          messages = event.channel.history(99, nil, message_id)
+          event.channel.prune(messages.count + 1, true)
+        rescue ArgumentError
+          reply = event.message.reply('You cannot purge messages older than 2 weeks, plip!')
+        rescue RestClient::BadRequest
+          reply = event.message.reply('Please give me a valid message ID, plip! (e.g. purge 0123456789)')
         end
 
         delete_request(event.message, reply, 10) if reply
