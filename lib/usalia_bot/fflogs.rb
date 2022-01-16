34175b0f85a770d71abf6a4f504c446c00ce4b5b
Matt Antonelli <contact@raelys.com>
2022-01-16 23:12:46 +0000

    Rework FFLogs integration
    Adds the log report channel as a configurable ID. Removes references to
    log "owner".

--- lib/usalia_bot/fflogs.rb
+++ lib/usalia_bot/fflogs.rb
@@ -2,6 +2,7 @@ module UsaliaBot
   module FFLogs
     extend self
 
+    FFLOGS_CHANNEL_ID = CONFIG.fflogs_channel_id.freeze
     FFLOGS_API_URL = "https://www.fflogs.com/v1/reports/guild/#{CONFIG.fflogs_guild}?api_key=#{CONFIG.fflogs_api_key}".freeze
     FFLOGS_REPORT_URL = 'https://www.fflogs.com/reports'.freeze
     FFLOGS_THUMBNAIL = 'https://www.fflogs.com/img/common/ff-logo.png'.freeze
@@ -10,12 +11,9 @@ module UsaliaBot
       reports = new_reports
       return if reports.empty?
 
-      channels = Redis.hgetall('fflogs-channels')
-
       reports.each do |report|
-        owner = report[:owner].downcase
-        channel = bot.channel(channels[owner])
-        send_embed(owner, channel, report)
+        channel = bot.channel(FFLOGS_CHANNEL_ID)
+        send_embed(channel, report)
         Redis.hset('fflogs-reports', report[:id], '')
       end
     end
@@ -24,18 +22,18 @@ module UsaliaBot
     def new_reports
       time = ((Time.now.to_f - 86400) * 1000).to_i
       response = RestClient.get("#{FFLOGS_API_URL}&start=#{time}")
-      reports = JSON.parse(response.body, symbolize_names: true).sort_by { |report| report[:start] }
-      reports.reject do |report|
-        !Redis.hexists?('fflogs-channels', report[:owner].downcase) || Redis.hexists?('fflogs-reports', report[:id])
-      end
+
+      JSON.parse(response.body, symbolize_names: true)
+        .sort_by { |report| report[:start] }
+        .reject { |report| Redis.hexists?('fflogs-reports', report[:id]) }
     end
 
-    def send_embed(owner, channel, report)
+    def send_embed(channel, report)
       url = "#{FFLOGS_REPORT_URL}/#{report[:id]}"
 
       channel.send_embed do |embed|
         embed.colour = 0x218aa6
-        embed.description = "New report from **#{owner}**:\n#{url}"
+        embed.description = url
         embed.timestamp = Time.at(report[:start] / 1000)
 
         embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: FFLOGS_THUMBNAIL)
